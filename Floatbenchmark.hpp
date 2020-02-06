f086aad484791a786f3cb215d92ba24c538bf85b
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-06 16:42:29 -0500

    Added Floatbenchmark.cpp file to check operation of floating point

---
+++ Floatbenchmark.hpp
@@ -0,0 +1,13 @@
+//
+//  Floatbenchmark.hpp
+//  
+//
+//  Created by Sabin on 2/6/20.
+//
+
+#ifndef Floatbenchmark_hpp
+#define Floatbenchmark_hpp
+
+#include <stdio.h>
+
+#endif /* Floatbenchmark_hpp */
