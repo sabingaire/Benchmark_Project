ba2969349e72f97be18a5ab6e0f651e6c62d2c72
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-06 16:59:17 -0500

    Added addition function

--- Floatbenchmark.cpp
+++ Floatbenchmark.cpp
@@ -7,4 +7,10 @@ void float_addition(float a, float b);
 void float_multiplication(float a, float b);
 void float_division(float a,float b);
 
-
+void float_addition(float a=1.23e+4 , float b=12.34e+2){
+    long long i =0;
+    while (i <100000000000){
+        a+=b;
+        i++;
+    }
+}
