50445a8bd2f78d90fee09643cfea27e56b978b79
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-06 15:37:00 -0500

    Wrote the function for integer addition

--- Integerbenchmark.cpp
+++ Integerbenchmark.cpp
@@ -7,3 +7,10 @@ void int_addition(int a, int b);
 void int_multiplication(int a, int b);
 void int_division(int a,int b);
 
+void int_addition(int a=6 , int b=12){
+    long long i =0;
+    while (i <100000000000){
+        a+=b;
+        i++;
+    }
+}
