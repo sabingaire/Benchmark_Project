f21841d0a4d129eca051ea8ddca0cee351d2c075
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-06 19:15:07 -0500

    Syntax error

--- Harddrivebenchmark.cpp
+++ Harddrivebenchmark.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 
 void write_harddrive(){
-char val ='10';
+char val ='6';
 ofstream ifile;
 ifile.open("input.txt");
     long long i=0;
 while(i<250000000){
     ifile.write(&val,4);
-    i++
+    i++;
 }
 ifile.close();
 }
@@ -20,7 +20,7 @@ ifile.close();
 void read_harddrive(){
   char val;
   ifstream ifile;
-  myfile.open("input.txt");
+  ifile.open("input.txt");
     long long i=0;
     while(i<250000000){
   
@@ -33,10 +33,12 @@ void read_harddrive(){
 int main() {
 time_t init,final;
 init=time(NULL);
-    write_harddrive;
-    read_harddrive;
+    write_harddrive();
+    read_harddrive();
 final=time(NULL);
+    cout<<"The time for harddrive is "
 cout<<final-init<<endl;
+
 
     return 0;
     
